7e3b88853588461b8453d846ace0c95428ddb7fc
Javier Ramírez <javier@humandecode.com>
2024-03-13 14:01:17 -0300

    Initial commit with Create React App.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import logo from "./img/logo192.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="font-comic-neue font-bold">CSS is awesome!</h1>
+        <img src={logo} className="logo" alt="A shocked monkey puppet." />
+      </header>
+    </div>
+  );
+}
+
+export default App;
