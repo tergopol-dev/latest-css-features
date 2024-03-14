8dede972e44f30ba2e06301ff6c4efcd31b5907e
Javier Ramírez <javier@humandecode.com>
2024-03-14 15:41:25 -0300

    WIP.

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,34 @@ import logo from "./img/logo192.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 className="font-comic-neue font-bold">CSS is awesome!</h1>
-        <img src={logo} className="logo" alt="A shocked monkey puppet." />
+    <main className="layout">
+      <nav className="side-nav">
+        {/* <ul>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+          <li>4</li>
+        </ul> */}
+      </nav>
+      <header className="header">
+        <div className="container">
+          <h1 className="font-comic-neue font-bold title-indent font-lg">
+            <span className="font-xl">
+              <span className="animate-random-color">C</span>
+              <span className="animate-random-color">S</span>
+              <span className="animate-random-color">S</span>
+            </span>{" "}
+            is <br />
+            <em>awesome!</em>
+          </h1>
+          <img
+            src={logo}
+            className="intro animate-intro"
+            alt="A shocked monkey puppet."
+          />
+        </div>
       </header>
-    </div>
+    </main>
   );
 }
 
